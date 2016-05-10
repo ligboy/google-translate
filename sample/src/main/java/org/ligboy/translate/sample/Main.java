71805885278f0b09fe1067eea8afbc13597f5db3
ligboy <ligboy.liu@heysroad.com>
2016-05-10 16:28:13 +0800

    add travis

--- sample/src/main/java/org/ligboy/translate/sample/Main.java
+++ sample/src/main/java/org/ligboy/translate/sample/Main.java
@@ -1,11 +1,27 @@
 package org.ligboy.translate.sample;
 
+
+import org.ligboy.translate.Translate;
+import org.ligboy.translate.model.TranslateResult;
+
 /**
  * @author ligboy
  */
 public class Main {
 
     public static void main(String[] args) {
+        final Translate translate = new Translate();
+
+        try {
+            translate.refreshTokenKey();
+
+            TranslateResult translateResult = translate.translate("哈哈哈哈哈", Translate.SOURCE_LANG_AUTO, "en");
+
+            System.out.println(translateResult);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
